faa0a6d70dc1fd7d92857a209c9a64f26959ae36
Udaypal Aarkoti <uaarkoti@gmail.com>
2015-07-28 20:46:43 -0700

    Changed the port to 9090

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker kill app > /dev/null 2>&1
 docker rm app > /dev/null 2>&1
-docker run -d --name app -p 9999:8080 uday/acmecorp-app
+docker run -d --name app -p 9999:9090 uday/acmecorp-app
 sleep 3
 curl -v http://localhost:9999
